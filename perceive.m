4964956b7137f5d9a83fb7feb823903f000203be
neumann-wj <julian.neumann@charite.de>
2021-05-19 14:30:30 +0200

    added single hemisphere Survey plots

--- perceive.m
+++ perceive.m
@@ -788,40 +788,53 @@ for a = 1:length(files)
                     writetable(T,fullfile(hdr.fpath,[hdr.fname '_run-LFPMontage_Peaks.csv']));
                     
                     figure('Units','centimeters','PaperUnits','centimeters','Position',[1 1 40 20],'visible','off')
+                    
                     ir = perceive_ci([hdr.chan '_R'],channels);
                     subplot(1,2,2)
-                    p=plot(freq,pow(ir,:));
-                    set(p(find(bad(ir))),'linestyle','--')
-                    hold on
-                    plot(freq,nanmean(pow(ir,:)),'color','k','linewidth',2)
-                    xlim([1 35])
-                    plot(peaks(ir,1),peaks(ir,2),'LineStyle','none','Marker','.','MarkerSize',12)
-                    for c = 1:length(ir)
-                        if peaks(ir(c),1)>0
-                            text(peaks(ir(c),1),peaks(ir(c),2),[' ' num2str(peaks(ir(c),1),3) ' Hz'])
+                    if ~isempty(ir)
+                        p=plot(freq,pow(ir,:));
+                        set(p(find(bad(ir))),'linestyle','--')
+                        hold on
+                        plot(freq,nanmean(pow(ir,:)),'color','k','linewidth',2)
+                        xlim([1 35])
+                        plot(peaks(ir,1),peaks(ir,2),'LineStyle','none','Marker','.','MarkerSize',12)
+                        for c = 1:length(ir)
+                            if peaks(ir(c),1)>0
+                                text(peaks(ir(c),1),peaks(ir(c),2),[' ' num2str(peaks(ir(c),1),3) ' Hz'])
+                            end
                         end
+                        xlabel('Frequency [Hz]')
+                        ylabel('Power spectral density [uV^2/Hz]')
+                        title(strrep({hdr.subject,char(hdr.SessionDate),'RIGHT'},'_',' '))
+                        legend(strrep(channels(ir),'_',' '))
+                    else
+                        title('RIGHT HEMISPHERE MISSING')
+                        axis off
                     end
-                    xlabel('Frequency [Hz]')
-                    ylabel('Power spectral density [uV^2/Hz]')
-                    title(strrep({hdr.subject,char(hdr.SessionDate),'RIGHT'},'_',' '))
-                    legend(strrep(channels(ir),'_',' '))
                     il = perceive_ci([hdr.chan '_L'],channels);
+                    
                     subplot(1,2,1)
-                    p=plot(freq,pow(il,:));
-                    set(p(find(bad(il))),'linestyle','--')
-                    hold on
-                    plot(freq,nanmean(pow(il,:)),'color','k','linewidth',2)
-                    xlim([1 35])
-                    title(strrep({hdr.subject,char(hdr.SessionDate),'LEFT'},'_',' '))
-                    plot(peaks(il,1),peaks(il,2),'LineStyle','none','Marker','.','MarkerSize',12)
-                    xlabel('Frequency [Hz]')
-                    ylabel('Power spectral density [uV^2/Hz]')
-                    for c = 1:length(il)
-                        if peaks(il(c),1)>0
-                            text(peaks(il(c),1),peaks(il(c),2),[' ' num2str(peaks(il(c),1),3) ' Hz'])
+                    if ~isempty(il)
+                        p=plot(freq,pow(il,:));
+                        set(p(find(bad(il))),'linestyle','--')
+                        hold on
+                        plot(freq,nanmean(pow(il,:)),'color','k','linewidth',2)
+                        
+                        xlim([1 35])
+                        title(strrep({hdr.subject,char(hdr.SessionDate),'LEFT'},'_',' '))
+                        plot(peaks(il,1),peaks(il,2),'LineStyle','none','Marker','.','MarkerSize',12)
+                        xlabel('Frequency [Hz]')
+                        ylabel('Power spectral density [uV^2/Hz]')
+                        for c = 1:length(il)
+                            if peaks(il(c),1)>0
+                                text(peaks(il(c),1),peaks(il(c),2),[' ' num2str(peaks(il(c),1),3) ' Hz'])
+                            end
                         end
+                        legend(strrep(channels(il),'_',' '))
+                    else
+                        title('LEFT HEMISPHERE MISSING')
+                        axis off
                     end
-                    legend(strrep(channels(il),'_',' '))
                     set(gcf,'visible','on')
                     savefig(fullfile(hdr.fpath,[hdr.fname '_run-LFPMontage.fig']))
                     pause(2)
