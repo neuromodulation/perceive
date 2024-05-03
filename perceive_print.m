2b4002dceb78e04e90ababf1f948029d02ff669c
Jonathan Vanhoecke <72917013+JonathanVHoecke@users.noreply.github.com>
2024-05-03 15:32:18 +0200

    split pdf and png print

--- perceive_print.m
+++ perceive_print.m
@@ -1,16 +1,25 @@
-function perceive_print(filename)
+function perceive_print(filename, mode)
+
+arguments
+    filename {mustBeText}
+    mode    {mustBeMember(mode,{'png','pdf'})} = 'png'
+end
 
 [fold,file,ext]=fileparts(filename);
 if ~exist(fold,'dir')
     mkdir(fold);
 end
-print(gcf,fullfile(fold,file),'-dpng','-r300','-opengl');
 
-% save the current orientation
-or=get(gcf,'PaperOrientation');
-% set landscape orientation
-set(gcf,'PaperOrientation','Landscape');
-% print with the 'bestfit' option to make the whole plot visible
-print(gcf,fullfile(fold,file),'-dpdf','-r80','-bestfit');
-% restore the orientation
-set(gcf,'PaperOrientation',or);
+switch mode
+    case 'png'
+        print(gcf,fullfile(fold,file),'-dpng','-r300','-opengl');
+    case 'pdf'
+        % save the current orientation
+        or=get(gcf,'PaperOrientation');
+        % set landscape orientation
+        set(gcf,'PaperOrientation','Landscape');
+        % print with the 'bestfit' option to make the whole plot visible
+        print(gcf,fullfile(fold,file),'-dpdf','-r80','-bestfit');
+        % restore the orientation
+        set(gcf,'PaperOrientation',or);
+end
