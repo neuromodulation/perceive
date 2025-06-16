24a0337294360c80c1a84475d35a052f52a734b3
Jojo Vanhoecke <72917013+JojoVh@users.noreply.github.com>
2025-06-16 12:07:35 +0200

    test GroupHistory

--- MATLABtests/MOCK_Create_GroupHistory.m
+++ MATLABtests/MOCK_Create_GroupHistory.m
@@ -1,4 +1,5 @@
 for i = 2:3
-    GroupHistory = perceive_GroupHistory(sprintf('Report_Json_Session_Report_MOCK%d.json', i));
+     js = perceive_GroupHistory(sprintf('Report_Json_Session_Report_MOCK%d.json', i));
+     GroupHistory = js.GroupHistory;
     save(sprintf('Report_Json_Session_Report_MOCK%d_GroupHistory.mat', i),"GroupHistory")
 end
