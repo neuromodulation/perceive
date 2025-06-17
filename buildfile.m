75845bdb874dc17126d78ad98c1a87a8fb22e94e
Jojo Vanhoecke <72917013+JojoVh@users.noreply.github.com>
2025-06-17 11:14:05 +0200

    add buildUtilities

--- buildfile.m
+++ buildfile.m
@@ -1,8 +1,48 @@
-function plan = buildfile
-    import matlab.buildtool.tasks.CodeIssuesTask
-    import matlab.buildtool.tasks.TestTask
+function plan = buildfile()
 
     plan = buildplan(localfunctions);
-    plan("check") = CodeIssuesTask;
-    plan("test") = TestTask;
+    
+    plan.DefaultTasks = "test";
+    
+    plan("package").Dependencies = "publishDoc";
+
+    plan("publishDoc").Dependencies = "test";
+    
+    plan("test").Dependencies = "check";
 end
+
+function checkTask(context)
+    % Temporarily add buildUtilities to the path (remove it at the end of the function)
+    oldPath = addpath(fullfile(context.Plan.RootFolder,"buildUtilities"));
+    raii = onCleanup(@()(path(oldPath)));
+    
+    codecheckToolbox(context.Plan.RootFolder);
+end
+
+function testTask(context)
+    % Temporarily add buildUtilities to the path (remove it at the end of the function)
+    oldPath = addpath(fullfile(context.Plan.RootFolder,"buildUtilities"));
+    raii = onCleanup(@()(path(oldPath)));
+    
+    testToolbox()
+end
+
+function publishDocTask(context)
+    % Generate HTML files
+
+    % Temporarily add buildUtilities to the path (remove it at the end of the function)
+    oldPath = addpath(fullfile(context.Plan.RootFolder,"buildUtilities"));
+    raii = onCleanup(@()(path(oldPath)));
+
+    gendocToolbox(context.Plan.RootFolder)
+end
+
+function packageTask(context)
+    % Package the toolbox in an MLTBX, just incrementing build.  Note that GitHub action calls packageToolbox directly.
+
+    % Temporarily add buildUtilities to the path (remove it at the end of the function)
+    oldPath = addpath(fullfile(context.Plan.RootFolder,"buildUtilities"));
+    raii = onCleanup(@()(path(oldPath)));
+
+    packageToolbox("build")
+end 
