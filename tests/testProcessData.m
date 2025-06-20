8b4490c8559501d365140bfe397c67d9df38f0a3
Jojo Vanhoecke <72917013+JojoVh@users.noreply.github.com>
2025-06-20 16:17:15 +0200

    update test suite

--- tests/testProcessData.m
+++ tests/testProcessData.m
@@ -26,54 +26,14 @@ classdef testProcessData < matlab.unittest.TestCase
                     sprintf('Mismatch in test file %d', i));
             end
         end
-        function testPerceiveModularPerceive(testCase)
-            for i = 1:numel(testCase.testFiles)
-                testFile = testCase.testFiles{i};
-                % Create two temporary folders
-                fix1 = matlab.unittest.fixtures.TemporaryFolderFixture;
-                fix2 = matlab.unittest.fixtures.TemporaryFolderFixture;
-                testCase.applyFixture(fix1);
-                testCase.applyFixture(fix2);
-
-                folder1 = fix1.Folder;
-                folder2 = fix2.Folder;
-
-                % Run your functions
-                addpath(pwd);
-                % Go to folder1
-                cd(folder1);
-                disp("Now in folder1: " + pwd);
-                perceive(testFile);  % Perceive
-
-                cd(folder2);
-                disp("Now in folder2: " + pwd);
-                perceiveModular(testFile);  % Perceive post-hackathon
-
-                % Compare folder contents
-                files1 = dir(fullfile(folder1, '**', '*'));
-                files2 = dir(fullfile(folder2, '**', '*'));
-
-                % Filter out directories
-                files1 = files1(~[files1.isdir]);
-                files2 = files2(~[files2.isdir]);
-
-                % Compare file names
-                names1 = sort({files1.name});
-                names2 = sort({files2.name});
-                testCase.verifyEqual(names1, names2, 'File names differ');
-
-                % Compare file contents
-                for k = 1:numel(names1)
-                    f1 = fullfile(folder1, names1{k});
-                    f2 = fullfile(folder2, names2{k});
-                    testCase.verifyTrue(isequal(fileread(f1), fileread(f2)), ...
-                        sprintf('File content mismatch: %s', names1{k}));
-                end
-
-
-
-            end
+        function RunMock2(testCase)
+            testPerceiveModularPerceive(testCase, testCase.testFiles{2})
         end
+
+        function RunMock3(testCase)
+            testPerceiveModularPerceive(testCase, testCase.testFiles{3})
+        end
+        
     end
 end
 
